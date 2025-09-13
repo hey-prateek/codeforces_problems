d7b447907af729a8f334b5b48168a3ff99b4297b
hey-prateek <prateektiwari010799@gmail.com>
2025-09-13 21:03:49 +0530

    13-sep codeforces contest

---
+++ SublimeSequence.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SublimeSequence {
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+        int tc=sc.nextInt();
+
+        while(tc-->0)
+        {
+            int x=sc.nextInt(), n=sc.nextInt();
+            System.out.println(n%2==0 ? 0 : x);
+        }
+        sc.close();
+    }
+    
+}
